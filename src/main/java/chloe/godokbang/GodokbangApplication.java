18b774c526484074568e73c0a6618bd1214477ad
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-06-06 14:59:53 +0900

    Create Project

---
+++ src/main/java/chloe/godokbang/GodokbangApplication.java
@@ -0,0 +1,13 @@
+package chloe.godokbang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GodokbangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GodokbangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chloe/godokbang/GodokbangApplicationTests.java
@@ -0,0 +1,13 @@
+package chloe.godokbang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GodokbangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
