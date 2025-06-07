a83aa71286328ae1da46fabaf0cc92835e7811c7
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-06-07 17:30:30 +0900

    feat: create repository interface for User entity

---
+++ src/main/java/chloe/godokbang/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package chloe.godokbang.repository;
+
+import chloe.godokbang.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    boolean existByEmail(String email);
+    boolean existByNickname(String nickname);
+    Optional<User> findByEmail(String email);
+}
