d0fb5257cae3ed52a76fe3f608b4def9d9ffc37d
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-06-07 20:49:31 +0900

    feat: configure password encryption and Spring Security settings

---
+++ src/main/java/chloe/godokbang/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package chloe.godokbang.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+       http
+               .csrf(AbstractHttpConfigurer::disable)
+               .authorizeHttpRequests(auth -> auth
+                       .requestMatchers("/login", "/join").permitAll()
+                       .anyRequest().authenticated()
+               )
+               .formLogin(form -> form
+                       .loginPage("/login")
+                       .loginProcessingUrl("/login")
+                       .usernameParameter("email")
+                       .defaultSuccessUrl("/", true)
+                       .permitAll()
+               )
+               .logout(logout -> logout
+                       .logoutUrl("/logout")
+                       .logoutSuccessUrl("/login?logout")
+                       .permitAll()
+               );
+       return http.build();
+    }
+}
