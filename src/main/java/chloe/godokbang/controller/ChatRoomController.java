dc303dbed99527ca5e09659024f1e3b3b805ef3a
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-06-12 16:54:27 +0900

    refactor: add import UUID

--- src/main/java/chloe/godokbang/controller/ChatRoomController.java
+++ src/main/java/chloe/godokbang/controller/ChatRoomController.java
@@ -16,6 +16,8 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.UUID;
+
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/chat")
