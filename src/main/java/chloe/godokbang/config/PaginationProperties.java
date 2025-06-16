de5e36e44853e934f50cb0a47252bbe0e42384e2
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-06-16 16:07:04 +0900

    fix: add @Setter annotation

--- src/main/java/chloe/godokbang/config/PaginationProperties.java
+++ src/main/java/chloe/godokbang/config/PaginationProperties.java
@@ -1,12 +1,14 @@
 package chloe.godokbang.config;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties(prefix = "app.pagination")
 @Getter
+@Setter
 public class PaginationProperties {
 
     private int pageSize;
