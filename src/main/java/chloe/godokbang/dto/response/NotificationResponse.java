0b3ed3b721828cd284a66ae8d8edb213c44ed29b
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-06-17 00:46:59 +0900

    feat: add id and createdAt in NotificationResponse for infinite scroll support

--- src/main/java/chloe/godokbang/dto/response/NotificationResponse.java
+++ src/main/java/chloe/godokbang/dto/response/NotificationResponse.java
@@ -14,35 +14,41 @@ import java.util.UUID;
 @NoArgsConstructor
 public class NotificationResponse {
 
+    private Long id;
     private UUID roomId;
     private String roomTitle;
     private String senderNickname;
     private String content;
     private MessageType type;
     private String sentAt;
+    private LocalDateTime createdAt;
     private boolean isRead;
 
     @Builder
-    public NotificationResponse(UUID roomId, String roomTitle, String senderNickname, String content, MessageType type,
-                                LocalDateTime sentAt, boolean isRead) {
+    public NotificationResponse(Long id, UUID roomId, String roomTitle, String senderNickname, String content, MessageType type,
+                                LocalDateTime sentAt, LocalDateTime createdAt, boolean isRead) {
+        this.id = id;
         this.roomId = roomId;
         this.roomTitle = roomTitle;
         this.senderNickname = senderNickname;
         this.content = content;
         this.type = type;
         this.sentAt = formatDateTime(sentAt);
+        this.createdAt = createdAt;
         this.isRead = isRead;
     }
 
-    public static NotificationResponse fromEntity(Notification notification, boolean isRead) {
+    public static NotificationResponse fromEntity(Notification notification) {
         return NotificationResponse.builder()
+                .id(notification.getId())
                 .roomId(notification.getMessage().getChatRoom().getId())
                 .roomTitle(notification.getMessage().getChatRoom().getTitle())
                 .senderNickname(notification.getMessage().getSender().getNickname())
                 .content(notification.getMessage().getContent())
                 .type(notification.getMessage().getType())
                 .sentAt(notification.getMessage().getSentAt())
-                .isRead(isRead)
+                .createdAt(notification.getCreatedAt())
+                .isRead(notification.isRead())
                 .build();
     }
 
